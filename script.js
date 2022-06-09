ed2245c2d81bf5161badc8f177bdb589d009d3c4
ProGamer2711 <64653188+ProGamer2711@users.noreply.github.com>
2022-06-09 09:18:09 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ class Tile {
 		this.y = y;
 		this.type = type;
 		this.number = number;
-		this.isClicked = false;
+		this.isUncovered = false;
 	}
 
 	static size = 50;
@@ -18,11 +18,13 @@ class Tile {
 	 * @param {CanvasRenderingContext2D} ctx
 	 */
 	draw(ctx) {
-		this.isClicked ? (ctx.fillStyle = "#00ff00") : (ctx.fillStyle = "#000000");
-		this.type === "mine"
-			? (ctx.fillStyle = "#ffff00")
-			: (ctx.fillStyle = "#000000");
+		ctx.fillStyle = "#696969";
 		ctx.strokeStyle = "#ffffff";
+
+		if (this.isUncovered) {
+			ctx.fillStyle = "#333333";
+			if (this.type === "mine") ctx.fillStyle = "#ffff00";
+		}
 
 		ctx.fillRect(this.x * Tile.size, this.y * Tile.size, Tile.size, Tile.size);
 		ctx.strokeRect(
@@ -32,7 +34,9 @@ class Tile {
 			Tile.size
 		);
 
-		if (this.type !== "mine") this.drawNumber(ctx);
+		if (this.isUncovered && this.type !== "mine") {
+			this.drawNumber(ctx);
+		}
 	}
 
 	/**
@@ -51,6 +55,29 @@ class Tile {
 			this.x * Tile.size + Tile.size / 2,
 			this.y * Tile.size + Tile.size / 2
 		);
+	}
+
+	/**
+	 * @param {Game} game
+	 */
+	uncover(game) {
+		this.isUncovered = true;
+
+		if (this.type === "mine") {
+			game.end("loss");
+		} else {
+			let board = game.board;
+			let emptySpacesLeft = false;
+
+			for (let i = 0; i < board.length; i++) {
+				for (let j = 0; j < board[i].length; j++) {
+					if (board[i][j].type === "mine") continue;
+					if (!board[i][j].isUncovered) emptySpacesLeft = true;
+				}
+			}
+
+			if (!emptySpacesLeft) game.end("win");
+		}
 	}
 }
 
@@ -72,24 +99,44 @@ class Game {
 		this.width = width;
 		this.height = height;
 		this.mineCount = mines;
+		this.isOver = false;
+		this.started = false;
 
-		this.generateMines();
-		this.generateBoard();
+		this.generateEmptyBoard();
+		this.draw();
 
-		canvas.addEventListener("click", e => {
+		this.clickListener = e => {
 			let x = Math.floor(e.offsetX / Tile.size);
 			let y = Math.floor(e.offsetY / Tile.size);
-			this.board[x][y].isClicked = true;
+
+			if (!this.started) {
+				this.started = true;
+				this.generateMines({ x, y });
+				this.generateBoard();
+			}
+
+			this.board[x][y].uncover(this);
 			this.draw();
-		});
+		};
+
+		canvas.addEventListener("click", this.clickListener);
 	}
 
-	generateMines() {
+	generateMines(startingTile) {
+		let clearTiles = [];
+		for (let i = startingTile.x - 1; i <= startingTile.x + 1; i++) {
+			for (let j = startingTile.y - 1; j <= startingTile.y + 1; j++) {
+				if (i < 0 || i >= this.width || j < 0 || j >= this.height) continue;
+				clearTiles.push({ x: i, y: j });
+			}
+		}
+
 		for (let i = 0; i < this.mineCount; i++) {
 			let x = Math.floor(Math.random() * this.width);
 			let y = Math.floor(Math.random() * this.height);
 
-			while (this.mines.some(mine => mine.x === x && mine.y === y)) {
+			while (this.mines.some(mine => mine.x === x && mine.y === y) || 
+				   clearTiles.some(tile => tile.x === x && tile.y === y)) {
 				x = Math.floor(Math.random() * this.width);
 				y = Math.floor(Math.random() * this.height);
 			}
@@ -98,6 +145,15 @@ class Game {
 				x,
 				y,
 			});
+		}
+	}
+
+	generateEmptyBoard() {
+		for (let i = 0; i < this.width; i++) {
+			this.board[i] = [];
+			for (let j = 0; j < this.height; j++) {
+				this.board[i][j] = new Tile(i, j, "none");
+			}
 		}
 	}
 
@@ -154,16 +210,27 @@ class Game {
 
 	draw() {
 		this.ctx.clearRect(0, 0, this.width * Tile.size, this.height * Tile.size);
-		for (let i = 0; i < this.width; i++) {
-			for (let j = 0; j < this.height; j++) {
+		for (let i = 0; i < this.board.length; i++) {
+			for (let j = 0; j < this.board[i].length; j++) {
 				this.board[i][j].draw(this.ctx);
 			}
 		}
+	}
+
+	end(reason) {
+		if (reason === "loss") {
+			alert("You lost!");
+		} else {
+			alert("You won!");
+		}
+
+		this.draw();
+		this.canvas.removeEventListener("click", this.clickListener);
 	}
 }
 
 let canvas = document.getElementById("canvas");
 
-let game = new Game(canvas, 10, 10, 10);
+let game = new Game(canvas, 5, 5, 3);
 
 game.draw();
